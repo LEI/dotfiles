aabde8d24893d3dd63b8f279a637836292364f5c
Guillaume Frichet <guillaume.frichet@gmail.com>
2014-10-04 16:04:44 +0200

    config managment

--- brew/brew.sh
+++ brew/brew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Homebrew utils
-# Uses files: utils, print, ask
+# Uses utils, print, ask
 
 install_homebrew () {
 	# If OS X and brew not already installed
@@ -26,12 +26,13 @@ brew_bundle () {
 		opts=$HOMEBREW_CASK_OPTS
 	fi
 
-	local file=init/$1file
+	local file=$1/.${1}
 
 	info "brew bundle $file" "..."
 
 	[ -f "$file" ] || warn "$file does not exists"
 
+	# & ?
 	if [ -f "$file" ] && silent brew bundle "$file" "$opts"; then
 		success "$1 successfully bundled !"
 		return 0
@@ -39,4 +40,23 @@ brew_bundle () {
 		fail "$1 failed to bundle !"
 		return 1
 	fi
+}
+
+do_cask () {
+	local cask=$1
+	((indent_level++))
+		if silent brew cask install $cask; then
+			((indent_level++))
+			local file=$DOTFILES_DIR/cask/$cask
+			local app="$(tr '[:lower:]' '[:upper:]' <<< ${cask:0:1})${cask:1}"
+			if silent source_file $file; then
+				success "$app" " "
+				open /Applications/$app.app
+				((indent_level--))
+			else
+				fail "$app" " "
+				((indent_level--))
+			fi
+		fi
+	((indent_level--))
 }
