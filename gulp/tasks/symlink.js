140c6453e26c3b6a30d68999d3fe689180b6e65d
Guillaume Frichet <LEI@lei.home>
2016-01-27 08:11:41 +0100

    +

--- gulp/tasks/symlink.js
+++ gulp/tasks/symlink.js
@@ -6,11 +6,25 @@ var fs = require('fs'),
     config = require('../config');
 
 module.exports = function symlink() {
-    var filePath = '/Users/LEI/.vim/bundle';
-    return utils.isSymlink(filePath).then(function (isSymlink) {
-        utils.log(filePath, 'is symlink ~>', isSymlink);
-        // resolve(filePath);
-    });
+    var paths = config.paths.symlinks;
+
+    var p = paths.reduce(function (promise, glob) {
+        return promise.then(function () {
+            return utils.isSymlink(glob.dest).then(function (isSymlink) {
+                utils.log(glob.dest, isSymlink);
+                // return isSymlink;
+            });
+        });
+    }, Promise.resolve());
+
+    return p;
+
+
+    // var filePath = '/Users/LEI/.vim/bundle';
+    // return utils.isSymlink(filePath).then(function (isSymlink) {
+    //     utils.log(filePath, 'is symlink ~>', isSymlink);
+    //     // resolve(filePath);
+    // });
 
     // return gulp.src(''); // <- config.paths.symlinks.toArguments('dest')
 };
