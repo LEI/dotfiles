484b49b3d637fc5a778b63d0b0c1fa1d0b2270c8
Guillaume Frichet <guillaume.frichet@gmail.com>
2016-03-27 18:19:17 +0000

    roles

---
+++ ansible/roles/hammerspoon/files/lib/caffeine.lua
@@ -0,0 +1,22 @@
+-- Caffeine
+-- github.com/cmsj/hammerspoon-config/blob/master/init.lua
+
+local caffeine = hs.menubar.new()
+
+function setCaffeineDisplay(state)
+    local result
+    if state then
+        caffeine:setIcon("caffeine-on.pdf")
+    else
+        caffeine:setIcon("caffeine-off.pdf")
+    end
+end
+
+function caffeineClicked()
+    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+if caffeine then
+    caffeine:setClickCallback(caffeineClicked)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
