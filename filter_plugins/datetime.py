948c7f375a1b25608d3f0c8f5ec932d7b8313a0d
Guillaume Frichet <guillaume.frichet@gmail.com>
2016-04-05 19:39:33 +0000

    py

--- filter_plugins/datetime.py
+++ filter_plugins/datetime.py
@@ -3,12 +3,14 @@ import datetime
 class FilterModule(object):
     def filters(self):
         return {
-            'datetime': format_timestamp
+            'timestamp': format_timestamp
         }
 
-def format_timestamp(value, format='default'):
-    if format == 'default':
-        format = '%Y-%m-%d@%H:%M:%S'
+def format_timestamp(value, format='datetime'):
+    if format == 'datetime':
+        format = '%Y-%m-%d %H:%M:%S'
     elif format == 'date':
         format = '%Y-%m-%d'
+    elif format == 'time':
+        format = '%H:%M:%S'
     return datetime.datetime.fromtimestamp(value).strftime(format)
