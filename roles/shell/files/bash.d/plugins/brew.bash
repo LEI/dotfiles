b93711a4a08e65400e5ae6fe6944426761a121e9
Guillaume Frichet <guillaume.frichet@gmail.com>
2016-06-07 17:11:41 +0200

    c

--- roles/shell/files/bash.d/plugins/brew.bash
+++ roles/shell/files/bash.d/plugins/brew.bash
@@ -11,10 +11,9 @@ HOMEBREW_CASK_OPTS="--appdir=/Applications" #--caskroom=/etc/Caskroom
 # Add tab completion for brew commands
 if hash brew 2>/dev/null; then
   BREW_PREFIX=$(brew --prefix)
-  # /usr/local/share/bash-completion/completion/* ?
-  if [[ -f "$BREW_PREFIX/share/bash-completion/bash_completion" ]]; then
-    source "$BREW_PREFIX/share/bash-completion/bash_completion"
-  # elif [[ -f "$BREW_PREFIX/etc/bash_completion" ]]; then
-  #   source "$BREW_PREFIX/etc/bash_completion"
+  # "/usr/local/share/bash-completion/completion/*"
+  # "$BREW_PREFIX/share/bash-completion/bash_completion"
+  if [[ -f "$BREW_PREFIX/etc/bash_completion" ]]; then
+    source "$BREW_PREFIX/etc/bash_completion"
   fi
 fi
