dc1379d52c4151227cf80534b9b5c57daabe862a
Guillaume Frichet <guillaume.frichet@gmail.com>
2016-10-02 11:45:21 +0200

    +

--- roles/shell/files/bash.d/aliases/main.bash
+++ roles/shell/files/bash.d/aliases/main.bash
@@ -101,6 +101,6 @@ for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
   alias "$method"="lwp-request -m '$method'"
 done
 
-alias ip-public="dig +short myip.opendns.com @resolver1.opendns.com"
+alias ip-public="dig +short myip.opendns.com @resolver1.opendns.com" # OS X
 alias ip-local="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
