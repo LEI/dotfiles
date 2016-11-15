da255576175324103e8316b2b77225f06e722cc5
Guillaume Frichet <guillaume.frichet@gmail.com>
2016-11-15 15:26:36 +0100

    +

--- roles/shell/files/bash.d/aliases/osx.bash
+++ roles/shell/files/bash.d/aliases/osx.bash
@@ -1,7 +1,14 @@
 [[ "$(uname -s)" == "Darwin" ]] || return
 
-# Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
-alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update'
+alias update='updatesystem; updatebrew; updatenpm; updateruby'
+# Get OS X Software Updates
+alias updatesystem='sudo softwareupdate -i -a'
+# Homebrew
+alias updatebrew='brew update; brew upgrade; brew cleanup'
+# Node packages
+alias updatenpm='npm install npm -g; npm update -g'
+# Ruby gems -n /usr/local/bin
+alias updateruby='sudo gem update --system; sudo gem update'
 
 # Canonical hex dump; some systems have this symlinked
 command -v hd > /dev/null || alias hd="hexdump -C"
