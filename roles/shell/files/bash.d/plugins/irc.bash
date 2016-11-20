3ecad785bdda26ad8735943093234e962ebffb3e
Guillaume Frichet <guillaume.frichet@gmail.com>
2016-11-21 00:16:13 +0100

    irc

--- roles/shell/files/bash.d/plugins/irc.bash
+++ roles/shell/files/bash.d/plugins/irc.bash
@@ -1,7 +1,6 @@
 # IRC
 
 ircdir="$HOME/irc"
-ircbin="${ircbin:-$ircdir/bin}"
 
 custom_network() {
   server="$1"
@@ -9,19 +8,29 @@ custom_network() {
   channels=("$@")
 }
 
+kirc() {
+  local ps="$(ps ux -A | awk '/\/bin\/connect|notifii[i]/ {print $2}')"
+  if [[ -n "$ps" ]]
+  then kill -9 $ps; pkill ii
+  fi
+}
+
 irc() {
   # Parse arguments
-  case "$@" in
-    -n" "*) # nick
-      shift
-      n="$1"
-      shift
-      ;;
-  esac
+  while true
+  do
+    case "$@" in
+      -n\ *) shift; n="$1"; shift ;;
+      --ssl\ *) shift; secure=1 ;;
+      *) break ;;
+    esac
+  done
+
   local args=("$@")
-  local connect="$ircbin/connect"
-  local iii="$ircbin/iii"
-  has tmux && iii="$ircbin/tmiii"
+  local connect="$ircdir/bin/connect"
+
+  local iii="$ircdir/bin/iii"
+  has tmux && iii="$ircdir/bin/tmiii"
   [[ -e "$iii" ]] || {
     echo "Not found: $iii"
     return 1
@@ -38,7 +47,7 @@ irc() {
 
   for network in $networks
   do
-    local hist= nick="${n:=$USER}" # l=sb r=eb
+    local hist= nick="${n:=$USER}" ssl="${secure-:}" # l=sb r=eb
     unset server port channels
     "$network" "${args[@]}"
 
@@ -54,26 +63,42 @@ irc() {
     local ps="$(ps -A ux | awk "/$pattern/ {print \$2}")"
     if [[ -z "$ps" ]]
     then
-      USER="$nick" "$connect" "$server" "${channels[@]}" # & wait "$!"
+      USER="$nick" ssl="$ssl" "$connect" "$server" "${channels[@]}" # & wait "$!"
       sleep 1
     elif [[ -n "$channels" ]]
     then
-      echo "Warning: connect already running for $server" # pkill ii or kill -9 \$(ps -A ux | awk '/$pattern/ {print \$2}')
-      printf "/j %s\n" "${channels[@]}" > "$ircdir/$server/in"
+      echo "Warning: connect already running for $server"
+      [[ -n "$channels" ]] && printf "/j %s\n" ${channels[@]} > "$ircdir/$server/in"
     fi
 
-    # [[ -z "$channels" ]] &&
+    # Notify
+    local notifiii="$ircdir/bin/notifiii"
+    if [[ -x "$notifiii" ]]
+    then
+      local notifps="$(ps -A ux | awk '/[notifii[i]/ {print $2}')"
+      if [[ -z "$notifps" ]]
+      then
+        env $opts "$notifiii" &>/dev/null &
+        # notifpid="$!" # TODO kill pid
+      fi
+    fi
+
     while ! test -p "$ircdir/$server/in"
     do sleep .3; done
     env $opts "$iii"
-    for channel in "${channels[@]}"
-    do
-      while ! test -f "$ircdir/$server/$channel/out"
-      do sleep .3; done
-      local chanopts="$opts c=$channel"
-      env $chanopts "$iii"
-    done
+
+    if [[ -n "$channels" ]]
+    then
+      for channel in "${channels[@]}"
+      do
+        while ! test -f "$ircdir/$server/$channel/out"
+        do sleep .3; done
+        local chanopts="$opts c=$channel"
+        env $chanopts "$iii"
+      done
+    fi
   done
+  # [[ -n "${notifpid:-}" ]] && kill "$notifpid"
 }
 
 # irc() {
