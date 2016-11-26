490c701bd6a0b836b7a94c57106c94bfbb84f6d3
Guillaume Frichet <guillaume.frichet@gmail.com>
2016-11-26 17:03:03 +0100

    a

--- roles/shell/files/bash.d/plugins/pb.bash
+++ roles/shell/files/bash.d/plugins/pb.bash
@@ -2,7 +2,10 @@
 # http://ptpb.pw/#shell-functions
 
 pb() {
-  curl -F "c=@${1:--}" http://ptpb.pw/
+  # expiry: -F sunset=120
+  # private: -F p=1
+  # vanity: http://ptpb.pw/~slug$
+  curl -F "c=@${1:--}" -F sunset=60 http://ptpb.pw/
 }
 
 # Copy the url of the uploaded paste
@@ -12,16 +15,19 @@ pbx() {
   then copy="xsel -l /dev/null -b"
   # elif has xclip # xclipboard
   # elif has pbcopy
-  else error "No clipboard manager"
+  else error "No clipboard manager found"
     return 1
   fi
   curl -sF "c=@${1:--}" -w "%{redirect_url}" 'http://ptpb.pw/?r=1' -o /dev/stderr | "$copy"
 }
+
+# Capture screenshot
 pbs () {
   gm import -window ${1:-root} /tmp/$$.png
   pbx /tmp/$$.png
 }
 
+# Record terminal
 pbs () {
   asciinema rec /tmp/$$.json
   pbx /tmp/$$.json
