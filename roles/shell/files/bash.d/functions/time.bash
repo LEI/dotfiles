da4ff38da79d68a5d18c2b4f1c7891650cccfd6e
Guillaume Frichet <guillaume.frichet@gmail.com>
2016-11-27 19:22:11 +0100

    args

---
+++ roles/shell/files/bash.d/functions/time.bash
@@ -0,0 +1,113 @@
+# Date and time utils
+
+uz() {
+  local r=${1#?} f=${1%"$r"}
+  if [[ "x$f" == "x0" ]]
+  then uz "$r"
+  else printf "%s\n" "$1"
+  fi
+}
+
+# Parse a duration string: 0Y0M0w0d0h1m0s -> 60
+get_seconds() {
+  local in=$1 ou=
+  local r= f=
+  while [[ -n "$in" ]]
+  do
+    r=${in#?}
+    f=${in%"$r"}
+    case $f in
+      Y) ou=$ou",31536000 " ;; # Year: 60 * 60 * 24 * 365
+      M) ou=$ou",2678400 " ;; # Month: 60 * 60 * 24 * 31
+      w) ou=$ou",604800 " ;; # week: 60 * 60 * 24 * 7
+      d) ou=$ou",86400 " ;; # day: 60 * 60 * 24
+      h) ou=$ou",3600 " ;; # hour: 60 * 60
+      m) ou=$ou",60 " ;; # minute
+      s) ou=$ou",1 " ;; # second
+      *) ou=$ou$f ;;
+    esac
+    in=$r
+  done
+  local p num times seconds=0
+  for p in $ou
+  do
+    num=$(uz "${p%,*}")
+    times=${p#*,}
+    seconds=$((seconds+times*num))
+  done
+  printf "%s\n" "$seconds"
+}
+
+# uz()(
+#   r=${1#?}
+#   f=${1%"$r"}
+#   if [ "x$f" = "x0" ]
+#   then uz "$r"
+#   else printf %s\\n "$1"
+#   fi
+# )
+# get_seconds() (
+#   in=$1 ou=;
+#   while [ -n "$in" ]
+#   do
+#     r=${in#?}
+#     f=${in%"$r"}
+#     case $f in
+#       m) ou=$ou",60 " ;;
+#       s) ou=$ou",1 " ;;
+#       *) ou=$ou$f ;;
+#     esac
+#     in=$r
+#   done
+#   seconds=0
+#   for p in $ou
+#   do
+#     num=$(uz "${p%,*}")
+#     times=${p#*,}
+#     seconds=$((seconds+times*num))
+#   done
+#   printf %s\\n "$seconds"
+# )
+
+# get_seconds() {
+#   local d=${1//m/'m '}
+#   d=${d//s/'s '}
+#   declare -A ls=([m]=60 [s]=1)
+#   local seconds=0
+#   read -a d <<<"$d"
+#   local i num ltr
+#   for i in "${!d[@]}"
+#     do num=${d[i]%%[ms]*}
+#     ltr=${d[i]##*[[:digit:]]}
+#     ((seconds+=num*ls[$ltr]))
+#   done
+#   printf "%s\n" "$seconds"
+# }
+
+# parse_duration() {
+#   # shopt -s extglob
+#   local str="$1"
+#   # local coef=()
+#   local t=0 s=0 m=0 h=0 d=0 w=0 M=0 Y=0
+#   for v in s m h d w M Y
+#   do
+#     [[ -z "$str" ]] && break
+#     local pattern="([0-9]+)$v"
+#     if [[ $str =~ $pattern ]]
+#     then eval $v="${BASH_REMATCH[1]}"; str="${str%${!v}$v}"
+#     fi
+#     # [[ -n "${!v}" ]] && [[ "${!v}" -gt 0 ]] \
+#     #   && t=$((t + ${!v} * ${v_coef))
+#   done
+#   if [[ -n "$str" ]]
+#   then error "$str: Unparsed arguments"; return 1
+#   fi
+#   # [[ -n "$s" ]] && [[ "$s" -gt 0 ]] && t=$((t + $s))
+#   # [[ -n "$m" ]] && [[ "$m" -gt 0 ]] && t=$((t + $m * 60))
+#   # [[ -n "$h" ]] && [[ "$h" -gt 0 ]] && t=$((t + $h * 60 * 60))
+#   # [[ -n "$d" ]] && [[ "$d" -gt 0 ]] && t=$((t + $d * 60 * 60 * 24))
+#   # [[ -n "$w" ]] && [[ "$w" -gt 0 ]] && t=$((t + $w * 60 * 60 * 24 * 7))
+#   # [[ -n "$M" ]] && [[ "$M" -gt 0 ]] && t=$((t + $M * 60 * 60 * 24 * 31))
+#   # [[ -n "$Y" ]] && [[ "$Y" -gt 0 ]] && t=$((t + $Y * 60 * 60 * 24 * 365))
+#   printf "%d" "$t"
+# }
--- roles/shell/files/bash.d/plugins/pb.bash
+++ roles/shell/files/bash.d/plugins/pb.bash
@@ -4,38 +4,47 @@
 
 PB_PROVIDER="https://ptpb.pw"
 PB_CURL_OPTS=""
+PB_USAGE="Usage: pb [-CRUShdpfuvx]"
 
+# set -e
 shopt -s extglob
 
 # local args="$@"
 arg() {
   local a="$1" # Argument name
-  local prefix next arg pattern="(-|--)($a)([a-z]*)*" # ( |=)
+  local prefix next arg pattern="(-|--)($a)*([a-z])?( |=)*" # ( |=)
   local var="${2:-}" # Variable name
   if [[ $args =~ $pattern ]]
   then # >&2 printf "MATCH %s\n" "${BASH_REMATCH[@]}"
     prefix="${BASH_REMATCH[1]}"
     a="${BASH_REMATCH[2]}"
     next="${BASH_REMATCH[3]}"
-    sep=" " # ${BASH_REMATCH[4]}}
-    arg="${args#${prefix}*${sep}}" arg="${arg%%${sep}*}"
+    sep="${BASH_REMATCH[4]:- }"
+    # >&2 echo "B4 arg: $arg, args: $args"
+    arg="${args#${prefix}*${sep}}" arg="${args%% -*}" # arg="${arg%%${prefix}*}" # arg="${arg%%${sep}*}"
     if [[ -n "$var" ]] && [[ "$var" != "bool" ]]
     then
+      arg="${arg#${prefix}${a}${sep}}"
+      # >&2 echo "arg: $arg, args: $args"
       if [[ -n "$arg" ]] && [[ "$arg" == ${prefix}* ]]
-      then error "${prefix}$a: Missing argument value (found: '$arg')"; return 1
+      # then error "${prefix}$a: Missing argument value (found: '$arg')"; return 1
+      then pberr "missing value -- $a" "found: $arg"; return 1
       fi
       if [[ -n "$arg" ]] && [[ "$args" != "$arg"* ]]
-      then eval "$var="$arg"" # If a value is found
-        # echo "-> eval $var=$arg ($a)" >&2
-      else error "${prefix}$a: Missing argument value"; return 1
+      then
+        # >&2 echo "$a -> eval $var=\"$arg\""
+        eval $var=\"$arg\"
+      # else error "${prefix}$a: Missing argument value"; return 1
+      else pberr "missing value -- $a"; return 1
       fi
+      # >&2 echo "prefix: $prefix, a: $a, sep: $sep, arg: $arg, next: '$next'"
       if [[ -n "$next" ]]
       then args="${args/${prefix}$a*${sep}$arg/${prefix}$next}" # $arg}"
       else args="${args#${prefix}$a${sep}$arg}" # args="${args#* }"
       fi
-    else # eval "$a=true"
+    else
+      # >&2 echo "$a -> eval ${var:-a}=true"
       eval "${var:-a}=true"
-      # echo "-> eval ${var:-a}=true ($a)" >&2
       args="${args#${prefix}$a}"
       if [[ -n "$next" ]]
       then args="${prefix}$args"
@@ -48,19 +57,19 @@ arg() {
   fi
 }
 
-larg() {
-  local arg="$1"
-  local sep="${2:- }"
-  args="${args#--$arg$sep}"
-  eval "$arg="${args%% *}""
-  args="${args#${!arg}}"
-}
+# larg() {
+#   local arg="$1"
+#   local sep="${2:- }"
+#   args="${args#--$arg$sep}"
+#   eval "$arg="${args%% *}""
+#   args="${args#${!arg}}"
+# }
 
 pb() {
   local args="$@"
   local url="$PB_PROVIDER"
   local opts=""
-  local create remove shorten update # Actions
+  local method create update remove shorten # Actions
   local debug private file handler uuid vanity sunset # Parameters
   while [[ -n "$args" ]]
   do local bool= # f= u= v= x=
@@ -68,32 +77,22 @@ pb() {
     # Trim leading and trailing [[:spaces:]]
     args="${args# }" args="${args% }"
     case "$args" in
-      -h*|--help*) error "Usage: pb [-CRSUpfruvx]"
-        error "create [f], remove <u>, shorten <<<, update <u>..."
-        return 1 ;;
+      ''|--\ *) break ;;
+      -h*|--help*) pberr; return 1 ;;
       -d*|--debug*) arg "d|debug" bool && debug="$bool" || return $? ;;
-      -p*) arg p bool && private="$bool" || return $? ;;
+      -p*|--private*) arg "p|private" bool && private="$bool" || return $? ;;
       -C*) arg C bool && create="$bool" || return $? ;;
+      -U*) arg U bool && update="$bool" || return $? ;;
       -R*) arg R bool && remove="$bool" || return $? ;;
       -S*) arg S bool && shorten="$bool" || return $? ;;
-      -U*) arg U bool && update="$bool" || return $? ;;
-      -f*) arg f file || return $? ;;
-      --file\ *) larg file || return $? ;;
-      --file=*) larg file = || return $? ;;
-      -u*) arg u uuid || return $? ;;
-      --uuid\ *) larg uuid || return $? ;;
-      --uuid=*) larg uuid = || return $? ;;
-      -v*) arg v vanity || return $? ;;
-      --vanity\ *) larg vanity || return $? ;;
-      --vanity=*) larg vanity = || return $? ;;
-      -x*) arg x sunset || return $? ;;
-      --sunset\ *) larg sunset || return $? ;;
-      --sunset=*) larg sunset = || return $? ;;
-      # -handler*) arg -handler handler || return $? ;;
-      --handler=*) args="${args#--handler=}" handler="${args%% *}" args="${args#$handler}" ;;
-      -*) error "${args%% *}: Invalid argument"; return 1 ;;
-      '') break ;;
-      *) echo "$args: Not matched!"; break ;;
+      -f*|--file*) arg "f|file" file || return $? ;;
+      -u*|--uuid*) arg "u|uuid" uuid || return $? ;;
+      -v*|--vanity*) arg "v|vanity" vanity || return $? ;;
+      -x*|--sunset*) arg "x|sunset" sunset || return $? ;;
+      --handler*) arg "handler" handler || return $? ;;
+      # --handler=*) args="${args#--handler=}" handler="${args%% *}" args="${args#$handler}" ;;
+      # -*) pberr "illegal option ${args%% *}"; return 1 ;;
+      *) pberr "illegal option -- $args"; return 1 break ;;
     esac
   done
 
@@ -104,47 +103,60 @@ pb() {
   # fi
 
   if [[ -z "$create$update$remove$shorten" ]]
-  then error "$@: Missing action"; return 1
+  then pberr "missing option -CURS"; return 1
   elif [[ "$create$update$remove$shorten" != true ]]
-  then error "$@: Invalid action"; return 1
+  then pberr "illegal action -CURS"; return 1
   fi
   case true in
     $create) # POST
+      if [[ -n "$uuid" ]]
+      then pberr "illegal option -- uuid"; return 1
+      fi
       [[ -n "$private" ]] && opts+=" -F p=1"
-      [[ -n "$sunset" ]] && opts+=" -F sunset=$sunset"
+      [[ -n "$sunset" ]] && opts+=" -F sunset=$(get_seconds $sunset)"
       [[ -n "$vanity" ]] && url+="/~$vanity"
-      opts+=" -F c=@${file:--}"
+      [[ -f "$file" ]] || [[ -z "$file" ]] && file="@${file:--}" # -e -f TODO
       ;;
-    $update) opts+=" -X PUT"
-      if [[ -z "$uuid" ]]
-      then error "-S: Invalid arguments (missing uuid)"; return 1
+    $update) method="PUT"
+      if [[ -n "$vanity" ]]
+      then pberr "illegal option -- vanity"; return 1
+      elif [[ -z "$uuid" ]]
+      then pberr "missing option -- uuid"; return 1
       else url+="/$uuid"
       fi
       [[ -n "$private" ]] && opts+=" -F p=1"
-      [[ -n "$sunset" ]] && opts+=" -F sunset=$sunset"
-      opts+=" -F c=@${file:--}"
+      [[ -n "$sunset" ]] && opts+=" -F sunset=$(get_seconds $sunset)"
+      [[ -f "$file" ]] || [[ -z "$file" ]] && file="@${file:--}"
       ;;
-    $remove) opts+=" -X DELETE"
+    $remove) method="DELETE"
       if [[ -n "$private$file$vanity$sunset" ]]
-      then error "-R: Too many arguments"; return 1
+      then pberr "illegal option -- p|f|v|s"; return 1
       elif [[ -z "$uuid" ]]
-      then error "-S: Invalid arguments (missing uuid)"; return 1
+      then pberr "missing option -- uuid"; return 1
       else url+="/$uuid"
       fi
       ;;
     $shorten) # POST
-      if [[ -n "$private$file$uuid$vanity$sunset" ]]
-      then error "-S: Too many arguments"; return 1
+      if [[ -n "$private$uuid$vanity$sunset" ]]
+      then pberr "illegal option -- p|u|v|s"; return 1
       # elif [[ -z "$link" ]]
-      # then error "-S: Invalid arguments (missing link)"; return 1
+      # then pberr "-S: Invalid arguments (missing link)"; return 1
       else url+="/u" # <<< $link
       fi
+      [[ -f "$file" ]] || [[ -z "$file" ]] && file="@${file:--}"
       # opts+=" -F c=@-"
-      opts+=" -F c=@${file:--}"
+      # opts+=" -F "c=$file""
       ;;
-    *) error "$@: Invalid action"; return 1 ;;
+    *) pberr "illegal action -- $@"; return 1 ;;
   esac
 
+  # if [[ -n "$sunset" ]] && [[ "$sunset" -gt 0 ]]
+  # then pberr "--sunset: Must be a number"; return 1
+  # else sunset="$(get_seconds $sunset)"
+  # fi
+
+  [[ -n "$file" ]] && opts+=" -F "c=$file""
+  [[ -n "$method" ]] && opts+=" -X $method"
   local curl_opts="$PB_CURL_OPTS $opts"
   if [[ -n "$handler" ]]
   then url+="?$handler=1"
@@ -154,9 +166,14 @@ pb() {
 
   # echo curl ${opts[@]} $url >&2
   if [[ -n "$debug" ]]
-  then echo curl $curl_opts $url
-  # else curl $curl_opts $url
+  then >&2 echo curl $curl_opts $url
+  else curl $curl_opts $url
   fi
+}
+
+pberr() {
+  [[ -n "${1:-}" ]] && error "pb: $1"
+  error "$PB_USAGE"
 }
 
 # Copy the url of the uploaded paste
@@ -177,7 +194,7 @@ pbx() {
 
 # Encrypt file with GPG symetric cipher
 pbg() { # Decrypt with curl <pasteurl> | gpg -d
-  gpg -o - -c "$file" | pbx -C # $@
+  gpg -o - -c "$1" | pbx -C # $@
 }
 
 # Capture screenshot
