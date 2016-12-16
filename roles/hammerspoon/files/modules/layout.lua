17a402c8dd1c6dcf730251b2f0f891206a7a7d7f
Guillaume Frichet <guillaume.frichet@gmail.com>
2016-12-16 12:28:49 +0100

    tap

--- roles/hammerspoon/files/modules/layout.lua
+++ roles/hammerspoon/files/modules/layout.lua
@@ -13,27 +13,26 @@ local function applyWindowLayout(screens)
         {"Mail", nil, screens[2]:name(), hs.layout.right50, nil, nil },
         {"Sequel Pro", nil, screens[2]:name(), hs.layout.center, nil, nil},
         {"Skype", nil, screens[1]:name(), hs.layout.left50, nil, nil },
-        {"Slack", nil, screens[2]:name(), hs.layout.center, nil, nil },
+        {"Slack", nil, screens[1]:name(), hs.layout.center, nil, nil },
         {"Terminal", nil, screens[1]:name(), nil, nil, screens[1]:fullFrame()},
-        {"X-Lite", nil, screens[1]:name(), hs.layout.right50, nil, nil },
     }
 
     hs.layout.apply(windowLayout)
 end
 
---[[function applicationWatcher(appName, eventType, appObject)
+function watchApps(appName, eventType, appObject)
+    local log = hs.logger.new('app')
     if (eventType == hs.application.watcher.activated) then
-        if (appName == "Terminal") then
-            local win = appObject:focusedWindow()
-            if win ~= nil then -- & isFullScreen?
-                win:setFullScreen(true)
-            end
-        end
+        log.i(appName, "appName")
+        log.i(appObject:focusedWindow(), "appFocusedWin")
+        -- if (appName == "Terminal") then
+        --     local win = appObject:focusedWindow()
+        --     if win ~= nil then -- & isFullScreen?
+        --         win:setFullScreen(true)
+        --     end
+        -- end
     end
 end
-
-local appWatcher = hs.application.watcher.new(applicationWatcher)
-appWatcher:start()--]]
 
 local function watchScreens()
     local screens = hs.screen.allScreens()
@@ -52,6 +51,9 @@ end
 
 return {
     init = function()
+        -- local appWatcher = hs.application.watcher.new(watchApps)
+        -- appWatcher:start()
+
         local screenWatcher = hs.screen.watcher.new(watchScreens)
         screenWatcher:start()
 
