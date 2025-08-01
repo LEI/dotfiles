7112c4066238ce4ae0d18dc7796194304a999de3
Guillaume <4112243+LEI@users.noreply.github.com>
2025-08-01 13:02:12 +0200

    fix: mise node version

--- home/dot_config/nvim/lua/config/init.lua
+++ home/dot_config/nvim/lua/config/init.lua
@@ -1,31 +1,44 @@
 vim.g.home = os.getenv('HOME')
 vim.g.tmp = os.getenv('TMPDIR') or '/tmp'
 
-local function get_node_config()
-  local node_version = ''
-  local node_prefix = ''
-  if vim.fn.executable('mise') == 1 then
+local mise_installs = vim.g.home .. '/.local/share/mise/installs'
+
+local function get_mise_node_version()
+  local node_version = vim.env.MISE_GLOBAL_NODE_VERSION
+  -- local match = vim.env.PATH:gsub('.*:' .. mise_installs .. '/node/([0-9\\.]+)/bin:.*', '%1')
+  -- if match ~= vim.env.PATH then
+  --   node_version = match
+  if node_version == '' then
+    node_version = nil
+  end
+  if not node_version and vim.fn.executable('mise') == 1 then
     local mise_node_cmd = "mise list node --cd='" .. vim.g.tmp .. "' --global --installed | awk '/^node / {print $2}'"
     node_version = vim.fn.system(mise_node_cmd):gsub('[\n\r]', '')
-    -- mise list node --installed --json --quiet | jq --raw-output 'last .install_path' # /bin/
-    if node_version ~= '' then
-      node_prefix = vim.g.home .. '/.local/share/mise/installs/node/' .. node_version .. '/bin/'
-    end
   end
-  -- Fallback to system node
-  if node_version == '' and (vim.fn.executable('node') == 1) then
+  return node_version ~= '' and node_version or nil
+end
+
+local function get_node_config()
+  local node_prefix = nil
+  local node_version = get_mise_node_version()
+  if node_version then
+    -- Use global mise node
+    node_prefix = mise_installs .. '/node/' .. node_version .. '/bin/'
+  elseif not node_version and (vim.fn.executable('node') == 1) then
+    -- Fallback to system node
     node_version = vim.fn.system('node --version'):gsub('[\n\r]', '')
     node_version = string.gsub(node_version, '^v', '')
   end
-  if node_version == '' then
-    vim.notify('node executable not found')
+  if not node_version or node_version == '' then
+    vim.notify('node version not found', vim.log.levels.WARN)
+    return {}
   end
-  local node_major_version = tonumber(string.match(node_version, '^%d+'))
+  if node_prefix and vim.fn.filereadable(node_prefix .. 'node') ~= 1 then
+    vim.notify('node prefix is not readable: ' .. node_prefix, vim.log.levels.WARN)
+  end
+  local node_major_version = node_version and tonumber(string.match(node_version, '^%d+')) or nil
   if node_major_version == nil or node_major_version < 20 then
-    vim.notify('node >=20 is required: ' .. node_version, vim.log.levels.ERROR)
-  end
-  if node_prefix ~= '' and vim.fn.filereadable(node_prefix .. 'node') ~= 1 then
-    vim.notify('node prefix is not readable: ' .. node_prefix, vim.log.levels.ERROR)
+    vim.notify('node >=20 is required' .. (node_version and ': ' .. node_version or ''), vim.log.levels.WARN)
   end
   return {
     prefix = node_prefix,
