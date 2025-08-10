29e763c1240f156af9b461a3505e54ec2c2507b7
Guillaume <4112243+LEI@users.noreply.github.com>
2025-08-10 15:38:23 +0200

    fix: misc [ci skip]

--- home/dot_config/tmux/plugins/tokyo-night-tmux/src/modify_executable_wb-git-status.sh
+++ home/dot_config/tmux/plugins/tokyo-night-tmux/src/modify_executable_wb-git-status.sh
@@ -2,5 +2,6 @@
 {{- /* chezmoi:modify-template */ -}}
 {{- .chezmoi.stdin
   | replaceAllRegex "# shellcheck disable=all" ""
+  | replaceAllRegex "url \\| awk -F '@\\|:'" "url | sed -e 's/^ssh:\\/\\///' | awk -F '@|:|/'"
   | replaceAllRegex "== \"gitlab.com\"" "!= \"\""
 -}}
