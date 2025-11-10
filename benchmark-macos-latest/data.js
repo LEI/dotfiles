c8abda1fe907d02b0c7884d06daa93db279b4df0
github-action-benchmark <github@users.noreply.github.com>
2025-11-10 01:33:25 +0000

    deploy: add bash benchmark result (macos-latest) for 7687f1bd23c52099470b5b410d74e1563b695692 [ci skip]

---
+++ benchmark-macos-latest/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1762738405804,
+  "repoUrl": "https://github.com/LEI/dotfiles",
+  "entries": {
+    "Bash benchmark (macos-latest)": [
+      {
+        "commit": {
+          "author": {
+            "email": "4112243+LEI@users.noreply.github.com",
+            "name": "Guillaume",
+            "username": "LEI"
+          },
+          "committer": {
+            "email": "4112243+LEI@users.noreply.github.com",
+            "name": "Guillaume",
+            "username": "LEI"
+          },
+          "distinct": true,
+          "id": "7687f1bd23c52099470b5b410d74e1563b695692",
+          "message": "fix: ci",
+          "timestamp": "2025-11-10T02:29:56+01:00",
+          "tree_id": "62211a39305a03383ebf7687da405750a65db646",
+          "url": "https://github.com/LEI/dotfiles/commit/7687f1bd23c52099470b5b410d74e1563b695692"
+        },
+        "date": 1762738404386,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bash average startup time",
+            "value": 0.104,
+            "unit": "Second"
+          },
+          {
+            "name": "bash initial startup time",
+            "value": 0.39,
+            "unit": "Second"
+          }
+        ]
+      }
+    ]
+  }
+}
