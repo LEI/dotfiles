0a75f3f17f03dad9161678a06cbae0d5dd713123
github-action-benchmark <github@users.noreply.github.com>
2025-11-11 07:13:27 +0000

    deploy: add bash benchmark result (ubuntu-latest) for 3c621c80d3ae6473a66bd53261bc712ae2350e5e

--- benchmark-ubuntu-latest/data.js
+++ benchmark-ubuntu-latest/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762823193299,
+  "lastUpdate": 1762845207422,
   "repoUrl": "https://github.com/LEI/dotfiles",
   "entries": {
     "Bash benchmark (ubuntu-latest)": [
@@ -67,6 +67,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "bash initial startup time",
             "value": 0.19,
+            "unit": "Second"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "4112243+LEI@users.noreply.github.com",
+            "name": "Guillaume",
+            "username": "LEI"
+          },
+          "committer": {
+            "email": "4112243+LEI@users.noreply.github.com",
+            "name": "Guillaume",
+            "username": "LEI"
+          },
+          "distinct": true,
+          "id": "3c621c80d3ae6473a66bd53261bc712ae2350e5e",
+          "message": "feat: bin",
+          "timestamp": "2025-11-11T08:10:41+01:00",
+          "tree_id": "cb034fb6f43b2429b698600d7c2bad808ed3f690",
+          "url": "https://github.com/LEI/dotfiles/commit/3c621c80d3ae6473a66bd53261bc712ae2350e5e"
+        },
+        "date": 1762845206916,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bash average startup time",
+            "value": 0.08,
+            "unit": "Second"
+          },
+          {
+            "name": "bash initial startup time",
+            "value": 0.13,
             "unit": "Second"
           }
         ]
