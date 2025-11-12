b4945ea34d2b94e670984179f96658d280abfbb6
Guillaume <4112243+LEI@users.noreply.github.com>
2025-11-12 03:12:04 +0100

    fix: coverage

--- coverage/index.js
+++
@@ -1,5 +0,0 @@
-var data = {files:[
-], merged_files:[{"link":"kcov-merged/index.html","title":"[merged]","summary_name":"[merged]","covered_class":"lineNoCov","covered":"0.0","covered_lines":"0","uncovered_lines":"0","total_lines" : "0"},
-]};
-var percent_low = 25;var percent_high = 75;
-var header = { "command" : "", "date" : "2025-11-12 00:58:12", "instrumented" : 0, "covered" : 0,};
