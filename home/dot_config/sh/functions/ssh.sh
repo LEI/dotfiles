3cb05ee970ad7e6a0f0881e0d83ab1c7f82e8cd0
Guillaume <4112243+LEI@users.noreply.github.com>
2026-03-09 08:21:52 +0100

    fix: agent [ci skip]

--- home/dot_config/sh/functions/ssh.sh
+++ home/dot_config/sh/functions/ssh.sh
@@ -1,3 +1,4 @@
+# shellcheck shell=bash
 sshcd() {
   local host="$1"
   local dir="${SSHCD_DIR[$host]:-}"
@@ -17,7 +18,6 @@ sshcd() {
 logs() {
   local name="$1"
   local lines="${2:-0}"
-  # shellcheck disable=SC2034
   local cmd="${SSHCD_LOGS[$name]:-}"
   if [ -z "$cmd" ]; then
     echo >&2 "logs: unknown host '$name'"
@@ -25,5 +25,8 @@ logs() {
     return 1
   fi
   local fmt="${SSHCD_LOGS_FMT[$name]:-cat}"
-  "$name" "$cmd" --lines="$lines" | $fmt
+  if [ "$lines" -gt 0 ]; then
+    cmd="${cmd/tail -f/tail -f --lines="$lines"}"
+  fi
+  "$name" "$cmd" | $fmt
 }
