e16fe226db3d4b7132e5c1d5abf9d77ee9648eab
Guillaume <4112243+LEI@users.noreply.github.com>
2026-03-28 20:12:11 +0100

    feat: npm

---
+++ home/dot_config/npm/init.sh
@@ -0,0 +1,9 @@
+if [ -d "$HOME/.node_modules/bin" ]; then
+  pathmunge "$HOME/.node_modules/bin" after
+fi
+if [ -n "${npm_config_prefix:-}" ] && [ -d "$npm_config_prefix/bin" ]; then
+  pathmunge "$npm_config_prefix/bin"
+fi
+if [ -n "${PNPM_HOME:-}" ] && [ -d "$PNPM_HOME" ]; then
+  pathmunge "$PNPM_HOME"
+fi
