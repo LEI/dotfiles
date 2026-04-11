46a3389c5a89a0a07dd4a625c1ecfdd1ae68cbd6
Guillaume <4112243+LEI@users.noreply.github.com>
2026-04-11 13:12:44 +0200

    fix: container [ci skip]

--- home/dot_config/docker/init.sh
+++
@@ -1,9 +0,0 @@
-if command -v lazydocker >/dev/null; then
-  alias D=lazydocker
-elif command -v oxker >/dev/null; then
-  alias D=oxker
-fi
-
-if command -v podman-tui >/dev/null; then
-  alias P=podman-tui
-fi
