6060730883f8461dfea5d8238604de6d1e6b1496
Guillaume <4112243+LEI@users.noreply.github.com>
2026-04-17 20:29:27 +0200

    fix: packages [ci skip]

--- home/dot_config/mise/.check.sh
+++ home/dot_config/mise/.check.sh
@@ -2,11 +2,19 @@
 
 set -euo pipefail
 
+# Required for yq on debian/ubuntu
+if [ -x /home/linuxbrew/.linuxbrew/bin/brew ]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
 if [ -f /etc/arch-release ] && { [ -f /.dockerenv ] || [ -f /run/.containerenv ]; }; then
   mise reshim # shims point to stale mise path after pacman updates
 fi
 
 mise_doctor() {
+  # set -x
+  # which yq
+  # yq --version
   mise doctor --json | yq --colors --prettyPrint '.warnings[]'
   # jq --raw-output '.warnings[]'
 }
