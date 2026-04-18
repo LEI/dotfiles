c3bf991287a6477f1193c414f959e7055d625ab7
Guillaume <4112243+LEI@users.noreply.github.com>
2026-04-18 17:38:38 +0200

    fix: shell [ci skip]

--- home/dot_config/bash/options.bash
+++ home/dot_config/bash/options.bash
@@ -2,15 +2,19 @@
 # Use `>|` to force redirection to an existing file
 set -o noclobber
 
-shopt -s autocd
 shopt -s cdspell
 shopt -s checkwinsize
 shopt -s cmdhist
-shopt -s dirspell
 shopt -s extglob
-shopt -s globstar
 shopt -s histappend
 shopt -s histreedit
 shopt -s histverify
 # shopt -s lithist
 shopt -s nocaseglob
+
+# Bash 4+ options
+if ((BASH_VERSINFO[0] >= 4)); then
+  shopt -s autocd
+  shopt -s dirspell
+  shopt -s globstar
+fi
