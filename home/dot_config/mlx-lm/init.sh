0654c3fba0627200b0bb595d3b79ad8d64e1f73f
Guillaume <4112243+LEI@users.noreply.github.com>
2026-06-06 06:26:37 +0200

    chore: mlx-lm

--- home/dot_config/mlx-lm/init.sh
+++ home/dot_config/mlx-lm/init.sh
@@ -1,20 +1,22 @@
-mlx_list() {
+# shellcheck shell=bash
+
+mlx-list() {
   mlx_lm manage --scan 2>/dev/null
 }
 
-mlx_logs() {
+mlx-logs() {
   tail -f "${HOMEBREW_PREFIX:?}/var/log/mlx-lm.log" "$@"
 }
 
-hf_list() {
+hf-list() {
   hf cache ls "$@"
 }
 
-hf_usage() {
+hf-usage() {
   hf cache ls --format=json | jq -r '.[] | "\(.repo_type)/\(.repo_id)\t\(.size_on_disk / 1073741824 * 100 | round / 100)G"'
 }
 
-hf_rm_all() {
+hf-cache-rm-all() {
   # shellcheck disable=SC2046
-  hf cache rm --yes $(hf_usage | cut -f1)
+  hf cache rm --yes $(hf-usage | cut -f1)
 }
